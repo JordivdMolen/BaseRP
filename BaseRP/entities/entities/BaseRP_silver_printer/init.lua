f52a79919bde53e862b6189846fa9fefe4a5c765
JMteam09 <JMgamerZzz@gmail.com>
2014-07-07 13:10:48 +0200

    Fixed jobs + enteties + shipments
    Removed the enteties that doest extist
    and the weapons and jobs

---
+++ BaseRP/entities/entities/BaseRP_silver_printer/init.lua
@@ -0,0 +1,149 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+ENT.SeizeReward = 950
+
+--Settings--
+local Color = Color( 181, 181, 181, 255 )
+local PrintTime1 = 100 --Range of the random timer--
+local PrintTime2 = 300
+local Health = 500
+local PrintAmountRange1 = 500
+local PrintAmountRange2 = 1000
+
+local PrintMore
+function ENT:Initialize()
+	self:SetModel("models/props_c17/consolebox01a.mdl")
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid(SOLID_VPHYSICS)
+	self:SetColor( Color )
+	self:SetNWInt("PrintB",Health)
+	local phys = self:GetPhysicsObject()
+	phys:Wake()
+
+	self.sparking = false
+	self.damage = Health
+	self.IsMoneyPrinter = true
+	timer.Simple(math.random(PrintTime1, PrintTime2), function() PrintMore(self) end)
+
+	self.sound = CreateSound(self, Sound("ambient/levels/labs/equipment_printer_loop1.wav"))
+	self.sound:SetSoundLevel(52)
+	self.sound:PlayEx(1, 100)
+end
+
+function ENT:OnTakeDamage(dmg)
+	if self.burningup then return end
+
+	self.damage = (self.damage or 100) - dmg:GetDamage()
+	if self.damage <= 0 then
+		local rnd = math.random(1, 10)
+		if rnd < 3 then
+			self:BurstIntoFlames()
+		else
+			self:Destruct()
+			self:Remove()
+		end
+	end
+	Health = self.damage
+	self:SetNWInt("PrintB",Health)
+end
+
+function ENT:Destruct()
+	local vPoint = self:GetPos()
+	local effectdata = EffectData()
+	effectdata:SetStart(vPoint)
+	effectdata:SetOrigin(vPoint)
+	effectdata:SetScale(1)
+	util.Effect("Explosion", effectdata)
+	DarkRP.notify(self:Getowning_ent(), 1, 4, DarkRP.getPhrase("money_printer_exploded"))
+end
+
+function ENT:BurstIntoFlames()
+	local stopBurst = hook.Run("moneyPrinterCatchFire", self)
+	if stopBurst == true then return end
+
+	DarkRP.notify(self:Getowning_ent(), 0, 4, DarkRP.getPhrase("money_printer_overheating"))
+	self.burningup = true
+	local burntime = math.random(8, 18)
+	self:Ignite(burntime, 0)
+	timer.Simple(burntime, function() self:Fireball() end)
+end
+
+function ENT:Fireball()
+	if not self:IsOnFire() then self.burningup = false return end
+	local dist = math.random(20, 280) -- Explosion radius
+	self:Destruct()
+	for k, v in pairs(ents.FindInSphere(self:GetPos(), dist)) do
+		if not v:IsPlayer() and not v:IsWeapon() and v:GetClass() ~= "predicted_viewmodel" and not v.IsMoneyPrinter then
+			v:Ignite(math.random(5, 22), 0)
+		elseif v:IsPlayer() then
+			local distance = v:GetPos():Distance(self:GetPos())
+			v:TakeDamage(distance / dist * 100, self, self)
+		end
+	end
+	self:Remove()
+end
+
+PrintMore = function(ent)
+	if not IsValid(ent) then return end
+
+	ent.sparking = true
+	timer.Simple(3, function()
+		if not IsValid(ent) then return end
+		ent:CreateMoneybag()
+	end)
+end
+
+function ENT:CreateMoneybag()
+	if not IsValid(self) or self:IsOnFire() then return end
+
+	local MoneyPos = self:GetPos()
+
+	local amount = math.random(PrintAmountRange1,PrintAmountRange2)
+
+	local prevent, hookAmount = hook.Run("moneyPrinterPrintMoney", self, amount)
+	if prevent == true then return end
+
+	amount = hookAmount or amount
+
+	if GAMEMODE.Config.printeroverheat then
+		local overheatchance
+		if GAMEMODE.Config.printeroverheatchance <= 3 then
+			overheatchance = 22
+		else
+			overheatchance = GAMEMODE.Config.printeroverheatchance or 22
+		end
+		if math.random(1, overheatchance) == 3 then self:BurstIntoFlames() end
+	end
+
+	local moneybag = DarkRP.createMoneyBag(Vector(MoneyPos.x + 15, MoneyPos.y, MoneyPos.z + 15), amount)
+	hook.Run("moneyPrinterPrinted", self, moneybag)
+	self.sparking = false
+	timer.Simple(math.random(PrintTime1, PrintTime2), function() PrintMore(self) end)
+end
+
+function ENT:Think()
+
+	if self:WaterLevel() > 0 then
+		self:Destruct()
+		self:Remove()
+		return
+	end
+
+	if not self.sparking then return end
+
+	local effectdata = EffectData()
+	effectdata:SetOrigin(self:GetPos())
+	effectdata:SetMagnitude(1)
+	effectdata:SetScale(1)
+	effectdata:SetRadius(2)
+	util.Effect("Sparks", effectdata)
+end
+
+function ENT:OnRemove()
+	if self.sound then
+		self.sound:Stop()
+	end
+end
--- BaseRP/gamemode/config/addentities.lua
+++ BaseRP/gamemode/config/addentities.lua
@@ -8,13 +8,46 @@ DarkRP.createEntity("Gun lab", {
 	allowed = {TEAM_GUN, TEAM_JM}
 })
 
-DarkRP.createEntity("Normal Money Printer", {
-	ent = "money_printer",
+DarkRP.createEntity("Bronze Printer", {
+	ent = "BaseRP_bronze_printer",
 	model = "models/props_c17/consolebox01a.mdl",
 	price = 5000,
-	max = 99,
-	cmd = "buynormalprinter",
+	max = 6,
+	cmd = "buybronzeBaseRPprinter",
 })
+
+DarkRP.createEntity("Silver Printer", {
+	ent = "BaseRP_silver_printer",
+	model = "models/props_c17/consolebox01a.mdl",
+	price = 5000,
+	max = 6,
+	cmd = "buysilverBaseRPprinter",
+})
+
+DarkRP.createEntity("Gold Printer", {
+	ent = "BaseRP_gold_printer",
+	model = "models/props_c17/consolebox01a.mdl",
+	price = 5000,
+	max = 6,
+	cmd = "buygoldBaseRPprinter",
+})
+
+DarkRP.createEntity("Diamond Printer", {
+	ent = "BaseRP_diamond_printer",
+	model = "models/props_c17/consolebox01a.mdl",
+	price = 5000,
+	max = 6,
+	cmd = "buyDiamondBaseRPprinter",
+})
+
+DarkRP.createEntity("Rapidum Printer", {
+	ent = "BaseRP_rapidum_printer",
+	model = "models/props_c17/consolebox01a.mdl",
+	price = 5000,
+	max = 6,
+	cmd = "buyRapidumBaseRPprinter",
+})
+
 
 DarkRP.createEntity("Nuclear Money Printer", {
 	ent = "money_printer_nuclear",
@@ -32,129 +65,7 @@ DarkRP.createEntity("Universed Money Printer", {
 	cmd = "buyuniversedprinter",
 })
 
-DarkRP.createEntity("Bank", {
-	ent = "bank_vault",
-	model = "models/props_lab/harddrive02.mdl",
-	price = 1,
-	max = 99,
-	cmd = "buymoneybox",
-})
 
-AddCustomShipment("blowtorch", {
-	entity = "blowtorch",
-	model = "models/weapons/w_fn_scar_h.mdl",
-	price = 100,
-	amount = 5,
-	noship = false,
-	separate = true,
-	pricesep = 100,
-})
-
-AddCustomShipment("M9K Scar", {
-	entity = "m9k_scar",
-	model = "models/weapons/w_fn_scar_h.mdl",
-	price = 5000,
-	amount = 15,
-	noship = false,
-	allowed = {TEAM_GUN, TEAM_BD}
-})
-
-AddCustomShipment("M9K UMP45", {
-	entity = "m9k_ump45",
-	model = "models/weapons/w_hk_ump45.mdl",
-	price = 3000,
-	amount = 15,
-	noship = false,
-	allowed = {TEAM_GUN, TEAM_BD}
-})
-
-
-AddCustomShipment("M9K Striker12", {
-	entity = "m9k_striker12",
-	model = "models/weapons/w_striker_12g.mdl",
-	price = 2500,
-	amount = 15,
-	noship = false,
-	allowed = {TEAM_GUN, TEAM_BD}
-})
-
-
-AddCustomShipment("M9K M4a1 Iron", {
-	entity = "m9k_m4a1",
-	model = "models/weapons/w_m4a1_iron.mdl",
-	price = 7500,
-	amount = 15,
-	noship = false,
-	allowed = {TEAM_GUN, TEAM_BD}
-})
-
-AddCustomShipment("M9K FAMAS", {
-	entity = "m9k_famas",
-	model = "models/weapons/w_tct_famas.mdl",
-	price = 20000,
-	amount = 15,
-	noship = false,
-	allowed = {TEAM_GUN, TEAM_BD}
-})
-
-AddCustomShipment("M9K Ares Shrike", {
-	entity = "m9k_ares_shrike",
-	model = "models/weapons/w_ares_shrike.mdl",
-	price = 50000,
-	amount = 15,
-	noship = false,
-	allowed = {TEAM_GUN, TEAM_BD}
-})
-
-AddCustomShipment("M9K Jack Hammer", {
-	entity = "m9k_jackhammer",
-	model = "models/weapons/w_pancor_jackhammer.mdl",
-	price = 90000,
-	amount = 15,
-	noship = false,
-	allowed = {TEAM_GUN, TEAM_BD}
-})
-
-
-AddCustomShipment("M9K Double Barrel Shotgun", {
-	entity = "m9k_dbarrel",
-	model = "models/weapons/w_double_barrel_shotgun.mdl",
-	price = 500000,
-	amount = 15,
-	noship = false,
-	allowed = {TEAM_GUN, TEAM_BD}
-})
-
-DarkRP.createVehicle({
-        name = "Jeep",
-        model = "models/buggy.mdl",
-        price = 500
-})
-
-DarkRP.createVehicle({
-        name = "cadillac",
-        model = "models/buggy.mdl",
-        price = 50000
-})
-
-
-AddCustomShipment("M9K SVT 40", {
-	entity = "m9k_svt40",
-	model = "models/weapons/w_svt_40.mdl",
-	price = 100000,
-	amount = 15,
-	noship = false,
-	allowed = {TEAM_GUN, TEAM_BD}
-})
-
-AddCustomShipment("AK 47", {
-	entity = "weapon_ak472",
-	model = "models/weapons/w_rif_ak47.mdl",
-	price = 100000,
-	amount = 15,
-	noship = false,
-	allowed = {TEAM_GUN, TEAM_BD}
-})
 DarkRP.createEntity("-=[Big Base Gate]=-", {
         ent = "base_structure",
         model = "models/props_c17/consolebox05a.mdl",
@@ -186,109 +97,4 @@ DarkRP.createEntity("-=[Base Structure Destroyer]=-", {
         cmd = "buybasedestroyer",
 })
 
-DarkRP.createEntity("-=[Hobbo Printer]=-", {
-        ent = "hobbo_money_printer",
-        model = "models/props_c17/consolebox05a.mdl",
-        price = 0,
-        max = 4,
-        cmd = "buyhobboprinter",
-})
 
-DarkRP.createEntity("-=[Wood Printer]=-", {
-        ent = "topaz_money_printer",
-        model = "models/props_c17/consolebox05a.mdl",
-        price = 1000,
-        max = 999,
-        cmd = "buytopazprinter"
-})
-
- 
-DarkRP.createEntity("-=[Wizzard Printer]=-", {
-        ent = "amethyst_money_printer",
-        model = "models/props_c17/consolebox05a.mdl",
-        price = 5000,
-        max = 999,
-        cmd = "buyamethystprinter"
-})
- 
-DarkRP.createEntity("-=[Emerald Printer=-", {
-        ent = "emerald_money_printer",
-        model = "models/props_c17/consolebox05a.mdl",
-        price = 10000,
-        max = 999,
-        cmd = "buyemeraldprinter"
-})
- 
-DarkRP.createEntity("-=[Redstone Printer]=-", {
-        ent = "ruby_money_printer",
-        model = "models/props_c17/consolebox05a.mdl",
-        price = 25000,
-        max = 999,
-        cmd = "buyrubyprinter"
-})
- 
-DarkRP.createEntity("-=[Diamond Printer]=-", {
-        ent = "sapphire_money_printer",
-        model = "models/props_c17/consolebox05a.mdl",
-        price = 50000,
-        max = 999,
-        cmd = "buysapphireprinter"
-})
-
-
-DarkRP.createEntity("-=[Massa Printer (No VIP)]=-", {
-        ent = "mass_PRINT",
-        model = "models/props_c17/consolebox03a.mdl",
-        price = 100000,
-        max = 999,
-        cmd = "buymassaoneprinternormal"
-})
-
-
-DarkRP.createEntity("-=[NG_MoneyPrinter]=-", {
-        ent = "ng_money_printer",
-        model = "models/props_c17/consolebox01a.mdl",
-        price = 10000, -- (Printer+Cooler=Money Farm)Change this number to change the price of the Cooler in-game.
-        max = 999,
-        cmd = "buyngmoney"
-})
-
-DarkRP.createEntity("-=[NG_Cooler]=-", {
-        ent = "ng_cooler",
-        model = "models/props_c17/consolebox01a.mdl",
-        price = 100, -- (Printer+Cooler=Money Farm)Change this number to change the price of the Cooler in-game.
-        max = 999,
-        cmd = "buyngcooler"
-})
-
-DarkRP.createEntity("-=[NG_OverCloaker]=-", {
-        ent = "ng_overclocker",
-        model = "models/props_c17/consolebox01a.mdl",
-        price = 100, -- (Printer+Cooler=Money Farm)Change this number to change the price of the Cooler in-game.
-        max = 999,
-        cmd = "buyngcloakJM"
-})
-
-DarkRP.createEntity("-=[NG_Turner]=-", {
-        ent = "ng_tuner",
-        model = "models/props_c17/consolebox01a.mdl",
-        price = 100, -- (Printer+Cooler=Money Farm)Change this number to change the price of the Cooler in-game.
-        max = 999,
-        cmd = "buyngturnerng"
-})
-
-DarkRP.createEntity("-=[Coolant Cell]=-", {
-	ent = "money_coolant_cell",
-	model = "models/Items/battery.mdl",
-	price = 2000,
-	max = 20,
-	cmd = "coolantcell"
-})
-
-DarkRP.createEntity("-=[Printer Upgrade]=-", {
-	ent = "money_printer_upgrade",
-	model = "models/props_lab/box01a.mdl",
-	price = 2000,
-	max = 20,
-	cmd = "printerupgrade"
-})
